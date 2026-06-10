e96c2b63a300c706ae880aeeaa10e04dd1181d3f
Christian <christianpavilonis@outlook.com>
2026-06-10 11:54:17 -0500

    Fix Spin wasm CI check

--- crates/mocktioneer-adapter-spin/src/lib.rs
+++ crates/mocktioneer-adapter-spin/src/lib.rs
@@ -17,5 +17,6 @@ use spin_sdk::http_component;
 #[cfg(target_arch = "wasm32")]
 #[http_component]
 async fn handle(req: IncomingRequest) -> anyhow::Result<impl IntoResponse> {
-    edgezero_adapter_spin::run_app::<MocktioneerApp>(req).await
+    edgezero_adapter_spin::run_app::<MocktioneerApp>(include_str!("../../../edgezero.toml"), req)
+        .await
 }
